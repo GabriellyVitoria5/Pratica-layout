71d5151a3c650aabb7821a14b8ec210ff5ce3ac7
GabriellyVitoria5 <assuncaogabrielly5@gmail.com>
2024-11-29 20:34:12 -0300

    mostrar e esconder a senha feito manualmente

--- app/src/main/java/com/example/projetolayout/MainActivity.kt
+++ app/src/main/java/com/example/projetolayout/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projetolayout
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,20 +11,36 @@ import com.example.projetolayout.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var visiblePassword = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // inflar os componentes da interface
+        // Inflar os componentes da interface
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Mostrar e esconder a senha
+        binding.imgEyePassword.setOnClickListener {
+            if (visiblePassword) {
+                visiblePassword = false
+                binding.txtPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                binding.imgEyePassword.setBackgroundResource(R.drawable.eye_close_icon)
+            }
+            else{
+                visiblePassword = true
+                binding.txtPassword.inputType = InputType.TYPE_CLASS_TEXT
+                binding.imgEyePassword.setBackgroundResource(R.drawable.eye_open_icon)
+            }
+
+        }
+
     }
 }
